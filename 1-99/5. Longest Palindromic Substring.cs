b04ce4b7a09183f90d11c58ec551bb4ef7d03c30
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-09-24 12:50:25 +0800

    Update 5. Longest Palindromic Substring.cs

--- 1-99/5. Longest Palindromic Substring.cs
+++ 1-99/5. Longest Palindromic Substring.cs
@@ -1,3 +1,4 @@
+// bot-up
 public class Solution {
     int row = 0;
     int col = 0;
@@ -33,6 +34,57 @@ public class Solution {
     public void change(int i, int j){
         dp[i,j] = true;
         if(j-i+1>max)
+        {
+            max = j-i+1;
+            row = i;
+            col = j;
+        }
+    }
+}
+
+// top-down
+public class Solution {
+    int row = 0;
+    int col = 0;
+    int max = 0;
+    int[,] dp;
+    
+    public string LongestPalindrome(string s) {
+        if(s.Length==0) return "";
+        int n = s.Length;
+        dp = new int[n, n];
+        
+        TopDownDP(n-1,n-1,s);
+        return s.Substring(row,col-row+1);
+    }
+    
+    public void TopDownDP(int i,int j,string s)
+    {
+        if(i<0 || j<0) return;
+        if(dp[i,j]!=0) return;
+        
+        char a = s[i];
+        char b = s[j];
+        
+        if(j-i+1<=1)
+        {
+            if(a==b) update(i,j);
+            else dp[i,j] = -1;
+        }
+        else
+        {
+            if(dp[i+1,j-1]==0) TopDownDP(i+1,j-1,s);
+            if(a==b && dp[i+1,j-1]==1) update(i,j);
+            else dp[i,j] = -1;
+        }
+        TopDownDP(i-1,j,s);
+        TopDownDP(i-1,j-1,s);
+    }
+    
+    public void update(int i,int j)
+    {
+        dp[i,j] = 1;
+        if(j-i+1>=max)
         {
             max = j-i+1;
             row = i;
